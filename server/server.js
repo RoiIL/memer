0363558d71cda60ef0111b8654d1cad27716191a
RoiIL <roi.ial@gmail.com>
2019-07-02 23:57:45 +0300

    commenting the hard coded mem

--- server/server.js
+++ server/server.js
@@ -21,25 +21,25 @@ db.once('open', function() {
     console.log('MongoDb is connected!')
 });
 
-const Mem = require('./Mem');
-let mem = new Mem({
-    url: 'https://memerbucket.s3.us-east-2.amazonaws.com/MemsImages/PHOTO-2019-04-11-13-21-56.jpg',
-    captions: [
-        {
-            userName: 'IR',
-            caption: "Who's next?",
-            likes: 20,
-        },
-        {
-            userName: 'Kornilyo',
-            caption: "You're lucky, I'm in good mood...",
-            likes: 30,
-        },
-        {
-            userName: 'Razi',
-            caption: "Sara I found it!",
-            likes: 132538,
-        }
-    ]
-})
-mem.save();
+// const Mem = require('./Mem');
+// let mem = new Mem({
+//     url: 'https://memerbucket.s3.us-east-2.amazonaws.com/MemsImages/PHOTO-2019-04-11-13-21-56.jpg',
+//     captions: [
+//         {
+//             userName: 'IR',
+//             caption: "Who's next?",
+//             likes: 20,
+//         },
+//         {
+//             userName: 'Kornilyo',
+//             caption: "You're lucky, I'm in good mood...",
+//             likes: 30,
+//         },
+//         {
+//             userName: 'Razi',
+//             caption: "Sara I found it!",
+//             likes: 132538,
+//         }
+//     ]
+// })
+// mem.save();
